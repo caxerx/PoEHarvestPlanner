2417d20c85c522470bc05585af6b1322edc8cd76
Caxer Tsang <caxer.tsang@atg.ai>
2020-06-24 15:33:17 +0800

    Improve visibility

--- src/components/PlacementDisplay.vue
+++ src/components/PlacementDisplay.vue
@@ -6,7 +6,7 @@
       :key="`placement-${index}`"
       class="placement-cell"
     >
-      <div>{{ p.text }}</div>
+      <div class="placement-text" :style="textStyle(p)">{{ p.text }}</div>
     </div>
   </div>
 </template>
@@ -16,38 +16,69 @@ import Vue from "vue";
 import Layout from "@/layout/harvest-layout.json";
 import { calculateCellPosition } from "@/utils/cell-calc";
 import { CellPlacement } from "@/types/CellPlacement";
+import { isSeed } from "../utils/placement-util";
 
 export default Vue.extend({
   name: "PlacementDisplay",
   props: {
+    isConnecting: {
+      default: false
+    },
+    connectingSeedAlpha: {
+      default: 0.3
+    },
+    defaultItemAlpha: {
+      default: 0.7
+    },
     size: { type: [Number], default: 20 },
-    placement: {}
+    placement: {},
+    linkPoint: {
+      default: (): CellPlacement[] => []
+    }
   },
   data() {
     return {
       cellData: Layout,
-      backgroundColor: ["#9C27B0", "#F57F17", "#2196F3"]
+      backgroundColor: ["156, 39, 176", "245, 127, 23", "33, 150, 243"]
     };
   },
   methods: {
+    textStyle(placement: CellPlacement) {
+      return {
+        "z-index": this.linkPoint.includes(placement) ? 6 : null
+      };
+    },
     cellStyle(placement: CellPlacement) {
       const [xPos, yPos] = calculateCellPosition(
         [+placement.x, +placement.y],
         this.size
       );
-      return {
+      const style = {
         height: `${this.size}px`,
         width: `${this.size}px`,
         top: `${xPos}px`,
         left: `${yPos}px`,
-        "background-color": this.backgroundColor[placement.color ?? -1]
+        "background-color": `rgba(${
+          this.backgroundColor[placement.color ?? -1]
+        }, ${this.defaultItemAlpha})`,
+        opacity: 1
       };
+
+      if (this.isConnecting && isSeed(placement)) {
+        style.opacity = this.connectingSeedAlpha;
+      }
+      return style;
     }
   }
 });
 </script>
 
 <style scoped>
+.placement-text {
+  position: relative;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+    1px 1px 0 #000;
+}
 .placement-cell {
   pointer-events: none;
   border: 1px solid rgba(0, 0, 0, 0.5);
@@ -55,7 +86,6 @@ export default Vue.extend({
   padding: 0;
   position: absolute;
   user-select: none;
-  z-index: 3;
   display: flex;
   align-items: center;
   justify-content: center;
