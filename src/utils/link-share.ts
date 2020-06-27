9dc221a74af5b714da2f648e1e0d9ab49c2574c7
Caxer Tsang <webmaster@caxerx.com>
2020-06-27 18:51:44 +0800

    fix build fail

--- src/utils/link-share.ts
+++ src/utils/link-share.ts
@@ -1,14 +1,9 @@
 import gzip from "gzip-js";
-import { Base64, Base64_ } from "js-base64";
+import { Base64 } from "js-base64";
 export function getShare(content: object) {
-  return (Base64 as Base64New).fromUint8Array(gzip.zip(JSON.stringify(content))).replace(/\//g, "_");
+  return Base64.fromUint8Array(new Uint8Array(gzip.zip(JSON.stringify(content)))).replace(/\//g, "_");
 }
 
 export function fromShare(content: string) {
-  return JSON.parse(String.fromCharCode(...gzip.unzip((Base64 as Base64New).toUint8Array(content.replace(/_/g, "/")))));
-}
-
-export interface Base64New extends Base64_ {
-  fromUint8Array(arr: number[]): string;
-  toUint8Array(str: string): number[];
+  return JSON.parse(String.fromCharCode(...gzip.unzip(Base64.toUint8Array(content.replace(/_/g, "/")))));
 }
