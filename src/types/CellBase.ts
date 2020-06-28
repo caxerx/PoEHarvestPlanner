7c9c29fc2ae4fa1ea7d423ad98aef9843c3910f9
Caxer Tsang <webmaster@caxerx.com>
2020-06-29 03:27:03 +0800

    update typing

---
+++ src/types/CellBase.ts
@@ -0,0 +1,11 @@
+export interface CellColor {
+  wild: string | null;
+  vivid: string | null;
+  primal: string | null;
+  default: string | null;
+}
+
+export interface CellPosition {
+  x: number;
+  y: number;
+}
---
+++ src/types/VisualSettings.ts
@@ -0,0 +1,28 @@
+export class AreaElementVisualSetting {
+  alwaysShowArea = false;
+  areaBorderOpacity = 30;
+  areaBackgroundOpacity = 10;
+  placementOpacity = 60;
+}
+
+export class VisualSettings {
+  linkFilter: number | null = null;
+  linkOpacity = 70;
+  greyscaleTerrain = false;
+  alwaysShowLink = true;
+  tier1SeedOpacity = 20;
+  tier2SeedOpacity = 20;
+  tier3SeedOpacity = 40;
+  tier4SeedOpacity = 60;
+  storageOpacity = 60;
+  horticraftingStationOpacity = 60;
+  pylon = new AreaElementVisualSetting();
+  collector: AreaElementVisualSetting = { ...new AreaElementVisualSetting(), alwaysShowArea: true };
+  disperser = new AreaElementVisualSetting();
+}
+
+export interface SettingChangeRequest {
+  area: string | null;
+  key: string;
+  value: number | boolean | null;
+}
